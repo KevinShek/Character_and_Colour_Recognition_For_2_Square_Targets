5790d603b4d7083802cb080f9fe2f4250452ae8b
KevinShek <kevinshek7@outlook.com>
2022-09-13 16:53:52 +0100

    update to detection

--- detection_methods.py
+++ detection_methods.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 import time
-from math import sqrt
+from math import sqrt, atan2, cos, sin, pi
 
 class Detection:
     def __init__(self, config):
@@ -255,7 +255,7 @@ class Detection:
         current_index = None
         current_roi = None
         inner_switch = 1
-        image_copy = self.image.copy()
+        image_copy = image.copy()
 
         if output is not None and len(output[0]) != 0:
             outputs = output[0].tolist()
@@ -414,6 +414,30 @@ class Detection:
 
         return
 
+
+    def drawAxis(img, p_, q_, color, scale):
+        p = list(p_)
+        q = list(q_)
+        
+        ## [visualization1]
+        angle = atan2(p[1] - q[1], p[0] - q[0]) # angle in radians
+        hypotenuse = sqrt((p[1] - q[1]) * (p[1] - q[1]) + (p[0] - q[0]) * (p[0] - q[0]))
+        
+        # Here we lengthen the arrow by a factor of scale
+        q[0] = p[0] - scale * hypotenuse * cos(angle)
+        q[1] = p[1] - scale * hypotenuse * sin(angle)
+        cv2.line(img, (int(p[0]), int(p[1])), (int(q[0]), int(q[1])), color, 3, cv.LINE_AA)
+        
+        # create the arrow hooks
+        p[0] = q[0] + 9 * cos(angle + pi / 4)
+        p[1] = q[1] + 9 * sin(angle + pi / 4)
+        cv2.line(img, (int(p[0]), int(p[1])), (int(q[0]), int(q[1])), color, 3, cv.LINE_AA)
+        
+        p[0] = q[0] + 9 * cos(angle - pi / 4)
+        p[1] = q[1] + 9 * sin(angle - pi / 4)
+        cv2.line(img, (int(p[0]), int(p[1])), (int(q[0]), int(q[1])), color, 3, cv.LINE_AA)
+        ## [visualization1]
+
     
     def getOrientation(self, pts, img):
         # https://automaticaddison.com/how-to-determine-the-orientation-of-an-object-using-opencv/
@@ -427,7 +451,7 @@ class Detection:
         
         # Perform PCA analysis
         mean = np.empty((0))
-        mean, eigenvectors, eigenvalues = cv.PCACompute2(data_pts, mean)
+        mean, eigenvectors, eigenvalues = cv2.PCACompute2(data_pts, mean)
         
         # Store the center of the object
         cntr = (int(mean[0,0]), int(mean[0,1]))
@@ -435,7 +459,7 @@ class Detection:
         
         ## [visualization]
         # Draw the principal components
-        cv.circle(img, cntr, 3, (255, 0, 255), 2)
+        cv2.circle(img, cntr, 3, (255, 0, 255), 2)
         p1 = (cntr[0] + 0.02 * eigenvectors[0,0] * eigenvalues[0,0], cntr[1] + 0.02 * eigenvectors[0,1] * eigenvalues[0,0])
         p2 = (cntr[0] - 0.02 * eigenvectors[1,0] * eigenvalues[1,0], cntr[1] - 0.02 * eigenvectors[1,1] * eigenvalues[1,0])
         drawAxis(img, cntr, p1, (255, 255, 0), 1)
@@ -446,8 +470,8 @@ class Detection:
         
         # Label with the rotation angle
         label = "  Rotation Angle: " + str(-int(np.rad2deg(angle)) - 90) + " degrees"
-        textbox = cv.rectangle(img, (cntr[0], cntr[1]-25), (cntr[0] + 250, cntr[1] + 10), (255,255,255), -1)
-        cv.putText(img, label, (cntr[0], cntr[1]), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv.LINE_AA)
+        textbox = cv2.rectangle(img, (cntr[0], cntr[1]-25), (cntr[0] + 250, cntr[1] + 10), (255,255,255), -1)
+        cv2.putText(img, label, (cntr[0], cntr[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
         
         return angle
  
